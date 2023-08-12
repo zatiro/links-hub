3e2ca59bd033824f56b5f6849508255ea742f958
Tiago Cardoso <tialmeida26@gmail.com>
2023-08-12 11:46:11 -0300

    Nova configuração do eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:tailwindcss/recommended',
+    '@rocketseat/eslint-config/next',
+    'next/core-web-vitals',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js'],
+      parser: '@typescript-eslint/parser',
+    },
+  ],
+}
