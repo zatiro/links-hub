4367f0595ddd266bb3ad630051c42bfbe88b2956
Tiago Cardoso <tialmeida26@gmail.com>
2023-08-20 23:36:21 -0300

    Ajuste de responsividade

--- src/app/Links/page.tsx
+++ src/app/Links/page.tsx
@@ -3,7 +3,7 @@ import LinkCard from '@/components/Link/LinkCard'
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <ul className="grid grid-cols-2 gap-4 sm:grid-cols-1 lg:grid-cols-3">
+      <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <LinkCard
           area="Manufatura"
           categorie="Roteiro"
@@ -29,7 +29,7 @@ export default function Home() {
         ></LinkCard>
         <LinkCard></LinkCard>
         <LinkCard></LinkCard>
-      </ul>
+      </div>
     </main>
   )
 }
